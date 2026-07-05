69abe0a1c3e93feac5a8a79924b440d52455b400
tokyramarozaka <tokyramarozaka@gmail.com>
2026-07-05 14:25:07 +0300

    fix: strip down SubscriptionRequest to just user id

--- src/main/java/mg/tokimahery/rmz/endpoint/rest/controller/dto/SubscriptionRequest.java
+++ src/main/java/mg/tokimahery/rmz/endpoint/rest/controller/dto/SubscriptionRequest.java
@@ -1,10 +1,7 @@
 package mg.tokimahery.rmz.endpoint.rest.controller.dto;
 
-import java.time.Instant;
 import java.util.UUID;
 import lombok.Builder;
-import mg.tokimahery.rmz.model.SubscriptionStatus;
 
 @Builder
-public record SubscriptionRequest(
-    UUID id, Instant createdAt, SubscriptionStatus status, UUID userId) {}
+public record SubscriptionRequest(UUID userId) {}
